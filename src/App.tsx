48604a64148802d07fe5aba38e793b095efd0d3d
Robert MacCloy <robert.maccloy@gmail.com>
2025-03-19 12:22:21 -0700

    feat: Set up Tailwind and customize scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mx-auto max-w-7xl">
+      <div className="flex flex-col gap-4 mx-4 mt-4">
+        <h1 className="text-2xl font-bold">Component Demo</h1>
+        <div className="flex flex-col min-h-24 w-xl border border-dashed border-gray-300 rounded-md p-4">
+          <h2 className="text-lg font-bold">Display component here.</h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
